80bc3578bddb80f59ac85dc61f821b4072d56950
Gabriel Felix <gabrielfelxx@gmail.com>
2025-07-08 21:05:15 -0300

    fix: refactor code smell Long Parameter List using Introduction Parameter Object refactoring technique

--- src/main/java/org/example/controllers/StudyRegistryController.java
+++ src/main/java/org/example/controllers/StudyRegistryController.java
@@ -224,17 +224,33 @@ public class StudyRegistryController {
         System.out.println("Study Plan Added");
     }
 
-    private void getWeekInfo(){
+    private void getWeekInfo() {
         System.out.println("(Study Task Manager Week Set Up) Type the following info: String planName, String objectiveTitle, " +
                 "String objectiveDescription, String materialTopic, String materialFormat, String goal, String reminderTitle, " +
                 "String reminderDescription, String mainTaskTitle, String mainHabit, String mainCardStudy");
-        studyTaskManager.setUpWeek(getInput(), getInput(), getInput(), getInput(), getInput(), getInput(), getInput(), getInput(),
-                getInput(), getInput(), getInput());
+
+        WeekSetup weekSetup = new WeekSetup.Builder()
+                .planName(getInput())
+                .objectiveTitle(getInput())
+                .objectiveDescription(getInput())
+                .materialTopic(getInput())
+                .materialFormat(getInput())
+                .goal(getInput())
+                .reminderTitle(getInput())
+                .reminderDescription(getInput())
+                .mainTaskTitle(getInput())
+                .mainHabit(getInput())
+                .mainCardStudy(getInput())
+                .build();
+
+        studyTaskManager.setUpWeek(weekSetup);
     }
 
-    private void handleSetUpWeek(){
+
+    private void handleSetUpWeek() {
         getWeekInfo();
     }
+
 
     private void handleGetWeekResponsibilities(){
         List<String> responsibilities = studyTaskManager.getWeekResponsibilities();
